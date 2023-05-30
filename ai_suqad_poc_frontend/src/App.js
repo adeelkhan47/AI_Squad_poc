54964e66d19705fbc738c7571afb8ababcfdb03d
amir.sohail <amir.sohail@10pearls.com>
2023-05-30 19:52:25 +0500

    Updated CSS

--- ai_suqad_poc_frontend/src/App.js
+++ ai_suqad_poc_frontend/src/App.js
@@ -5,7 +5,6 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>Audio Uploader</h1>
       <AudioUploader />
     </div>
   );
--- ai_suqad_poc_frontend/src/components/AudioUploader.js
+++ ai_suqad_poc_frontend/src/components/AudioUploader.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import '../css/AudioUploader.css';
 
 const AudioUploader = () => {
-  const [audioPath, setAudioPath] = useState('');
   const [responseText, setResponseText] = useState('');
 
   const [selectedFile, setSelectedFile] = useState(null);
@@ -11,14 +11,19 @@ const AudioUploader = () => {
     setSelectedFile(event.target.files[0]);
   };
 
-  const handleUpload = () => {
+  const handleUpload = (e) => {
+    e.preventDefault();
+    setSelectedFile(null);
     if (selectedFile) {
       const formData = new FormData();
       formData.append('file', selectedFile);
 
       axios.post('http://127.0.0.1:5000/detector/speechtotext', formData)
         .then((response) => {
-          console.log(response.data); // Handle the response from Flask
+          console.log("resp",response); // Handle the response from Flask
+          setResponseText(response.data)
+          setSelectedFile(response.data);
+          
         })
         .catch((error) => {
           console.error(error);
@@ -27,14 +32,27 @@ const AudioUploader = () => {
   };
 
   return (
-    <div>
+    <div className="audio-uploader">
+      <h1>Accessibility</h1>
       <form onSubmit={handleUpload}>
-        <input type="file" accept="audio/*" onChange={handleFileSelect} />
-        <button type="submit" disabled={!selectedFile}>
+        <label htmlFor="file-input" className="file-label">
+          Choose an audio file:
+        </label>
+        <input
+          id="file-input"
+          type="file"
+          accept="audio/*"
+          onChange={handleFileSelect}
+        />
+        <button type="submit" disabled={!selectedFile} className="upload-btn">
           Upload
         </button>
       </form>
-      <textarea value={responseText} readOnly />
+      <textarea
+        className="response-textarea"
+        value={responseText}
+        readOnly
+      />
     </div>
   );
 };
